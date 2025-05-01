cbb3c656bca9f336f3c83d88241c36c558f062f5
Saurabh Ojha <ojha.saurabh81@gmail.com>
2025-05-01 17:28:21 +0530

    adding shadcn components - table, loader. Adding API interface and mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import "./App.css";
+import { getColumnConfig } from "./sections/results/columnConfig";
+import { useEffect } from "react";
+import { useState } from "react";
+import { getEntities } from "./api/api";
+
+import SidebarSection from "./sections/sidebar/SidebarSection";
+import Header from "./sections/header/Header";
+import QueryName from "./sections/queryInput/QueryName";
+import QueryInput from "./sections/queryInput/QueryInput";
+import ResultsGrid from "./sections/results/ResultsGrid";
 
 function App() {
-  return <>Main</>;
+  const [data, setData] = useState<any[]>([]);
+  const [columns, setColumns] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await getEntities("customers");
+      if (results.type === "success") {
+        const columns = getColumnConfig(results.data);
+        setColumns(columns);
+        setData(results.data);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="h-full w-dvw flex">
+      <SidebarSection />
+      <div className="grow h-dvh flex flex-col">
+        <Header />
+        <main className="box-border flex grow mx-4 mt-4 mb-4 flex-col gap-4">
+          <QueryName />
+          <QueryInput />
+          <ResultsGrid columns={columns} data={data} heightOffset={300} />
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+import "./main.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/:queryId" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
