2121c52be5f7cd8738f9402ab7c281f2e91c96a6
Saurabh Ojha <ojha.saurabh81@gmail.com>
2025-05-02 10:30:12 +0530

    adding route config, saving in local storage, adding buttons for export and copy

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import { QueriesProvider } from "./store/queryProvider";
 
 import "./main.css";
 import App from "./App.tsx";
+import InitialView from "./pages/initialView/InitialView.tsx";
+import QueryView from "./pages/queryView/QueryView.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueriesProvider>
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/query/:queryId" element={<App />} />
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<InitialView />} />
+            <Route path="/query" element={<InitialView />} />
+            <Route path="/query/:queryId" element={<QueryView />} />
+          </Route>
         </Routes>
       </QueriesProvider>
     </BrowserRouter>
